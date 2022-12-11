bef67fe5a5ab8e25ac77e0434f5b69ba5c488091
Karolina Marcińczak <carolyn.matusiak90@gmail.com>
2022-12-12 00:03:46 +0100

    classes added

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,10 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const listOfImages = document.querySelector("ul.gallery");
+const imagesToUl = images
+  .map(
+    (image) =>
+      `<li class="gallery__item"><img class="gallery__img" src="${image.url}" alt="${image.alt}"/></li>`
+  )
