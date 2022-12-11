f2b2430e561a35db090794b25b1025359374562c
Karolina Marcińczak <carolyn.matusiak90@gmail.com>
2022-12-12 00:27:30 +0100

    click listeners added

---
+++ js/task-04.js
@@ -0,0 +1,15 @@
+"use strict"; 
+
+let counterValue = 0;
+const plusButton = document.querySelector("[data-action=increment]");
+const minusButton = document.querySelector("[data-action=decrement]");
+
+plusButton.addEventListener("click", () => {
+    counterValue += 1;
+    document.querySelector("#value").textContent = counterValue;
+});
+
+minusButton.addEventListener("click", () => {
+    counterValue += 1;
+    document.querySelector("#value").textContent = counterValue;
+});
