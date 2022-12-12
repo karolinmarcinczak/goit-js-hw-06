b3e1ec91ebad56f571f0df6ea6826120c1eceda3
Karolina Marcińczak <carolyn.matusiak90@gmail.com>
2022-12-12 18:31:59 +0100

    updates on task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+"use strict";
 
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", makeSubmit);
+const loginData = {};
+
+function makeSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, pasword },
+    } = event.currentTarget;
+    if (email.value === "" || password.value === "") {
+        return alert("Please fill in all of the fields!");
+    };
+
+    loginData.email = email.value;
+    loginData.password = password.value;
+    console.log(loginData);
+    event.currentTarget.reset();
+
+}
