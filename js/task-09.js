9a26186e6b100f577f35e364a767c76089a1ae5a
Karolina Marcińczak <carolyn.matusiak90@gmail.com>
2022-12-12 18:45:57 +0100

    task 9 updates

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
+"use strict";
+
+const body = document.querySelector("body");
+const btnColorChange = document.querySelector("button.change-color");
+const spanColor = document.querySelector("span.color");
+
+btnColorChange.addEventListener("click", getRandomHexColor);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
