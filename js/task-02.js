98a60c4227d476ffe388a4f4b3c74183bbb9577e
Karolina Marcińczak <carolyn.matusiak90@gmail.com>
2022-12-14 14:18:01 +0100

    task2 corrected

--- js/task-02.js
+++ js/task-02.js
@@ -12,7 +12,7 @@ const ingredients = [
  const ingredientsList = document.querySelector("#ingredients");
 for (const ingredient of ingredients) {
   const item = document.createElement("li");
+  item.textContent = ingredient;
   item.classList.add("item");
-  item.textContent = "ingredient";
   ingredientsList.append(item);
 };
